a2fad4ef0aabb639cf63ea7b3d73331f8ab465da
Michael Jaros <mj@mj1.at>
2016-07-12 11:39:18 +0200

    code improvement, documentation, handling of module's persistDirectory setting fixed

--- AutoExportTemplatesAndFields/cli/import.php
+++ AutoExportTemplatesAndFields/cli/import.php
@@ -1,38 +1,66 @@
 #!/usr/bin/php
 <?php
 
-function main($argc, $argv) {
+/**
+ * entry point
+ */
+function main() {
 
     // bootstrap ProcessWire:
     require_once(__DIR__ . "/../../../../index.php");
+
+    // initialize logging:
     global $log;
 	$log = Wire::getFuel('log');
 
-    // handle command line:
+    // handle command line and invoke functionality accordingly:
+    
     $options=getopt('r:i',array('restore:','import'));
     
-    if(array_key_exists('i', $options) or array_key_exists('import', $options)) {
-        // do the import:
-        importAll();
-    } elseif( (array_key_exists('r', $options) and $restorePath = $options['r']) or 
-              (array_key_exists('restore', $options) and $restorePath = $options['restore'])
-              ) {
-       out("Restoring from '$restorePath'...");
-       restoreDB($restorePath);
-    } else {
-        usage();
+    try {
+        if(array_key_exists('i', $options) or array_key_exists('import', $options)) {
+            // backup DB and import field/template data:
+            importAll();
+        } elseif( (array_key_exists('r', $options) and $restorePath = $options['r']) or 
+                  (array_key_exists('restore', $options) and $restorePath = $options['restore'])
+                  ) {
+            // restore specified DB backup:
+            out("Restoring from '$restorePath'...");
+            restoreDB($restorePath);
+        } else {
+            usage();
+        }
+    } catch (Exception $e) {
+        err("An error occurred - aborting: " . $e->getMessage());
+        exit(1);
     }
 }
 
+/**
+ * outputs usage message
+ */
 function usage() {
     global $argv;
     echo "usage: " . $argv[0] . " [ -i | --import ] [ -r <path> | --restore <path> ]" ;
 }
 
+/**
+ * prints message to stdout and logs it via PWs $log
+ */
 function out($msg) {
     global $log;
 
 	$log->message($msg); 
+    echo "$msg\n";
+}
+
+/**
+ * prints message to stdout and logs it as an error via PWs $log
+ */
+function err($msg) {
+    global $log;
+
+    $log->error($msg);
     echo "$msg\n";
 }
 
@@ -45,22 +73,23 @@ function backupDB($backupPath) {
     $backup = $db->backups();
     $file = $backup->backup();
     if($file) {
-        print_r($backup->notes()); 
-        out("Database restored successfully.");
-    } else 
-        print_r($backup->errors()); 
-    return $file; 
+        return $file; 
+    } else {
+        throw new WireException("Backup failed: " . print_r($backup->errors(), true));
+    } 
 }
 
+/**
+ * restores database from the specified path using WireDatabaseBackup
+ * @param $restorePath
+ */
 function restoreDB($restorePath) {
     $db = Wire::getFuel('database');
     $backup = $db->backups();
     $success = $backup->restore($restorePath); 
-    if($success) {
-        print_r($backup->notes()); 
-        out("Database restored successfully.");
-    } else 
-        print_r($backup->errors()); 
+    if(!$success) {
+        throw new WireException("Restore failed: " . print_r($backup->errors(), true));
+    }
 }
 
 /**
@@ -68,7 +97,7 @@ function restoreDB($restorePath) {
  * @param $file file to import data from
  * @return array data for import that has been read and JSON-decoded from the specified file
  */
-function importData($directory, $filename) {
+function readAndDecodeData($directory, $filename) {
     $data = file_get_contents($directory . DIRECTORY_SEPARATOR . "$filename");
     
     if(FALSE === $data) {
@@ -86,24 +115,31 @@ function importAll() {
     global $log;
     $modules = Wire::getFuel('modules');
 
+    // get AutoExportTemplatesAndFields module:
     $moduleName = 'AutoExportTemplatesAndFields';
     $module = $modules->get($moduleName);
     if(!$module) {
-        echo "module '$moduleName' is not installed\n";
+        out("module '$moduleName' is not installed");
         exit(1);
     }
 
-    $persistDirectory = $module->data['persistDirectory'] or $module->getDefaultConfig()['persistDirectory'];
-    $persistDirectory =  $module->getDefaultConfig()['persistDirectory']; // TODO
+    // get persistDirectory configuration setting
+    $persistDirectory = $module->persistDirectory or $module->getDefaultConfig()['persistDirectory'];
+    if($persistDirectory==null or $persistDirectory=='') {
+        throw new WireException ("Invalid or empty persist directory setting");
+    }
 
+    // create backup of DB: 
     out("backing up DB...");
     $file=backupDB($persistDirectory);
     out("backup to '$file' complete.");
 
     out("starting manual import from '$persistDirectory'...");
 
+    // disable export for the duration of the import:
     $module->setExportDisabled(true);
 
+    // define which collection object and object class to use for import of each file:
     $importFunctionality = array (
             'fields.json' => array(
                 'collectionObject' => Wire::getFuel('fields'), 
@@ -119,21 +155,25 @@ function importAll() {
             ),
     );
 
+    // using above settings, read, decode, and import each file:
     try {
         foreach($importFunctionality as $file=>$importParameters) {
             out("importing data from '$file'...");
-            $data=importData($persistDirectory, $file);
+
+            // read and JSON decode data from file:
+            $data=readAndDecodeData($persistDirectory, $file);
             if(!$data) throw new WireException("file '$file': Invalid import data");
 
+            // make ProcessWire import the data:
             importGeneralData($importParameters['collectionObject'], $importParameters['exportableClassName'], $data);
         }
         out("manual import from '$persistDirectory' complete...");
     } catch (Exception $e) {
-        $log->error($e->getMessage()); 
-        echo $e->getMessage()."\n";
+        err($e->getMessage());
+    } finally {
+        // re-enable export:
+        $module->setExportDisabled(false);
     }
-    
-    $module->setExportDisabled(false);
 }
 
 /**
@@ -147,19 +187,28 @@ function importGeneralData($collectionObject, $exportableClassName, array $colle
 
     $sanitizer = Wire::getFuel('sanitizer');
 
+    // loop through each item of the collection:
     foreach($collectionData as $name => $data) {
 
+        // sanitize the item's name:
         $name = $sanitizer->name($name);
-        echo "importing $exportableClassName '$name'...\n"; // TODO
+        out("importing $exportableClassName '$name'...");
 
-        if($collectionObject->get($name)) { // existing object
+        // get or create the item object:
+        if($collectionObject->get($name)) { // use existing object
             $object=$collectionObject->get($name);
-        } else { // new object
+        } else { // create new object
             $object= new $exportableClassName($name);
         }
 
+        // configure the object according to import data:
         $object->setImportData($data);
+
+        // persist the object so it survives this invocation:
         if($exportableClassName=='Template') {
+            // special handling for templates: fieldgroups have to be saved
+            // first (code borrowed from
+            // ProcessTemplateExportImport::saveItem()):
             $fieldgroup = $object->fieldgroup;
             $fieldgroup->save();
             $fieldgroup->saveContext();
@@ -169,10 +218,11 @@ function importGeneralData($collectionObject, $exportableClassName, array $colle
                 $object->save();
             }
         } else {
+            // normal handling for fields, field groups:
             $object->save();
         }
     }
 }
 
-main($argc, $argv);
+main();
 ?>
